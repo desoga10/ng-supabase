68be38b4b152135854423130a99ea9a7fe958568
desoga10 <done2stay@gmail.com>
2023-05-19 16:19:10 +0100

    Made Updates

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   supabase: {
-    url: 'https://ezfnyllgsljtnchycmkh.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6Zm55bGxnc2xqdG5jaHljbWtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMxNTU1MDQsImV4cCI6MTk5ODczMTUwNH0.Z85MX93aoefYWD8e3Pk1M2QJfktQlTX34EKe82JsqpY',
+    url: 'https://shvzqawjyooqervvqkmj.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNodnpxYXdqeW9vcWVydnZxa21qIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ1MDU4MDAsImV4cCI6MjAwMDA4MTgwMH0.K7i7vhR0TosM-xmhK_8zjyX_ajfhD9WA_iZ9KKrxnM8',
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,8 @@ import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { APP_ROUTES } from './app/app.routes';
 import { provideRouter } from '@angular/router';
-import { AuthService } from './app/service/auth.service';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(APP_ROUTES),
-    AuthService,
-    importProvidersFrom(HttpClientModule),
-  ],
+  providers: [provideRouter(APP_ROUTES), [provideHttpClient()]],
 }).catch((err) => console.error(err));
